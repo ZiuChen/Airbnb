66df54a9106f0f82a513655081f5bd80c2bcc617
ZiuChen <chenziyu03@kuaishou.com>
2023-05-03 16:21:23 +0800

    chore: 清理冗余代码

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,12 +1,6 @@
-import { memo, useEffect } from 'react'
-import request from '@/service'
+import { memo } from 'react'
 
 const Home = memo(() => {
-  useEffect(() => {
-    request.get({ url: '/home/highscore' }).then((res) => {
-      console.log(res)
-    })
-  }, [])
   return <div>Home</div>
 })
 
